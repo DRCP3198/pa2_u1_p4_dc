b938f4a380ccda296f1562a3e4da286ea5c9277e
Dillan Coloma <renatocoloma2010@gmail.com>
2023-05-15 19:37:42 -0500

    Taller 4 Concepto ID and IoC

--- src/main/java/com/example/demo/Pa2U1P4DcApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4DcApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,10 +18,19 @@ public class Pa2U1P4DcApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 	
 		System.out.println("Mi primer proyecto");
+		
+		Profesor profe = new Profesor();
+		 profe.setApellido("Coloma");
+		 profe.setNombre("Dillan");
+		 profe.setCedula("1751457167");
+		 profe.setFechaNacimiento( LocalDateTime.now());
+		 System.out.println(profe);
 	}
 
 	/* Spring Framework:
 	 * Permite a construccion/ desarrollo de aplicaciones empresariales
 	 * basadas en Spring.
 	 */
+	
+	
 }
